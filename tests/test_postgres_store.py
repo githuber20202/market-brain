2a31de07e8d99233c8e60da80dba5ab9e5c98629
githuber20202 <59474973+githuber20202@users.noreply.github.com>
2026-09-23 18:03:32 +0300

    Add ATR context to plan tests

--- tests/test_postgres_store.py
+++ tests/test_postgres_store.py
@@ -32,7 +32,7 @@ class FakeProvider:
         return MarketSnapshot(
             symbol=symbol,last=self.last,prior_close=98.0,bid=self.last-0.01,ask=self.last+0.01,
             volume=2_000_000,avg_volume=1_000_000,vwap=100.0,open_price=99.0,
-            opening_range_high=100.8,opening_range_low=99.6,retest_low=100.0,benchmark_return_pct=0.5,
+            opening_range_high=100.8,opening_range_low=99.6,retest_low=100.0,atr14=5.0,atr14_pct=5.0,remaining_atr=5.0,remaining_atr_pct=5.0,benchmark_return_pct=0.5,
             catalyst_verified=True,catalyst_strength=0.9,data_age_seconds=1.0,
             source_id="ALPACA_SIP",authoritative=True,
         )
@@ -41,6 +41,10 @@ class FakeProvider:
 def planning_snapshot():
     return MarketSnapshot(symbol="TEST",last=100.8,prior_close=98.0,bid=100.79,ask=100.81,volume=2_000_000,
         avg_volume=1_000_000,vwap=100.0,open_price=99.0,opening_range_high=100.8,opening_range_low=99.6,retest_low=100.0,
+        atr14=5.0,
+        atr14_pct=5.0,
+        remaining_atr=5.0,
+        remaining_atr_pct=5.0,
         benchmark_return_pct=0.5,catalyst_verified=True,catalyst_strength=0.9)
 
 
